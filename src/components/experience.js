d01637b28a2ad56ddbac155670fa037cd4f284e3
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-18 16:20:31 -0600

    changed the color scheme and ensured uniform code

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Experience extends Component {
   render() {
@@ -9,7 +9,7 @@ class Experience extends Component {
           <p>{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
         <Cell col={8}>
-          <h4 style={{marginTop:'0px'}}>{this.props.jobName}</h4>
+          <h4 style={{marginTop:"0px"}}>{this.props.jobName}</h4>
           <p>{this.props.jobDescription}</p>
         </Cell>
       </Grid>
