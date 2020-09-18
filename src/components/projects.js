4ce8eb68e41aef2dded997647189e73310c0ab4d
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-18 16:21:44 -0600

    changed some color schemes, and ensured code uniformity

--- src/components/projects.js
+++ src/components/projects.js
@@ -30,7 +30,7 @@ class Projects extends Component {
 
           {/* Portfolio 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.imgur.com/wiuCJqi.gif) center / cover'}}></CardTitle>
+            <CardTitle style={{color: '#fff', height: '140px', background: 'url(https://i.imgur.com/wiuCJqi.gif) center / cover'}}></CardTitle>
             <CardText>
               React Portfolio: This is my living portfolio, based on my first portfolio that was more heavily JavaScript coded. I changed my portfolio into a React based application. It allows a user to see my bio, projects, education/resume information, and contact information.  Lead and only developer. Tools / languages used were the Create React app and React based coding, along with HTML, CSS/Bootstrap React for the styling. 
             </CardText>
@@ -49,7 +49,7 @@ class Projects extends Component {
         <div className="projects-grid">
         {/* MEAN/MERN 1 */}
         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.imgur.com/Ii9AqvH.gif) center / cover'}} ></CardTitle>
+            <CardTitle style={{color: '#fff', height: '100px', background: 'url(https://i.imgur.com/Ii9AqvH.gif) center / cover'}} ></CardTitle>
             <CardText>
             Employee Directory:This project was created with Random User Generator API to create a list of employees that would allow the user to search for certain employees based on name. It also allows a user to sort the list from the toggle buttons in the column. Lead and only developer. Tools / languages used were the Create React app, React based coding, use of integrating API’s with routes, HTML and CSS. 
             </CardText>
@@ -63,7 +63,7 @@ class Projects extends Component {
           </Card>
           {/* MEAN/MERN 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.imgur.com/KZEm4Mu.gif) center / cover'}} >React Project #2</CardTitle>
+            <CardTitle style={{color: '#fff', height: '100px', background: 'url(https://i.imgur.com/KZEm4Mu.gif) center / cover'}}></CardTitle>
             <CardText>
               Budget Tracker: This program gives the User an input field to give a name to the transaction and to enter in the amount. Then there are two buttons that allow the User to select add or subtract from budget. Lead and only developer. Tools / languages used were node.js, MongoDB, mongoose, express, service worker for online/offline capability, logger, compression, Heroku for deployment, HTML, CSS, and JavaScript. 
             </CardText>
@@ -77,7 +77,7 @@ class Projects extends Component {
           </Card>
           {/* MEAN/MERN 3 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.imgur.com/h7KxEdu.gif) center / cover'}} >React Project #3</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.imgur.com/h7KxEdu.gif) center / cover'}}></CardTitle>
             <CardText>
             Fitness Tracker: This project allows a user to enter in workout data that is either cardio or weights, pending the exercise type it will collect certain information from the user. The user can then save this information to the current workout DB or pick to create a new workout DB with new data. This data is stored and shown in a graph for the user to see their workout progress. Lead and only developer. Tools / languages used were Heroku for deployment, mongoose, MongoDB, express, logger, and JavaScript. 
             </CardText>
