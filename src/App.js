0bd0740f377c52a08358d808d362d8afd2d83ccc
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-18 16:38:29 -0600

    needed to make some edits to enusure the new font appeared

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: "none", color: "black"}} to="/">MyPortfolio</Link>} scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: "none", color: "black"}} to="/">My Portfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
