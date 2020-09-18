01f411a218db8070a015dbdccfe03bf156eadcc7
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-18 16:22:17 -0600

    updated color schemes and also ensured code uniformity

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-import Education from './education';
-import Experience from './experience';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
 
 
 
@@ -11,15 +11,15 @@ class Resume extends Component {
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{textAlign: "center"}}>
              
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Shelby Handfield</h2>
-            <h4 style={{color: 'grey'}}>Full Stack Web Developer</h4>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h2 style={{paddingTop: "2em"}}>Shelby Handfield</h2>
+            <h4 style={{color: "grey"}}>Full Stack Web Developer</h4>
+            <hr style={{borderTop: "3px solid #833fb2", width: "50%"}}/>
             <p>Full- stack web developer with an emphasis on back-end development utilizing skills gained from a psychology and criminal justice background to break into the world on the web. Recently acquired a certificate for Full Stack Web Development from the University of Denver. With flourishing skills in MVC Framework, MEAN/MERN Stack Development, API, deployment, Browser-based technologies, and Server-Side Development, to name a few. My purpose is to push myself to better my skills, knowledge, and to work with others to create experiences for users. The last project I completed, we were a team of five and we developed a React based application to help pet owners to enter in information regarding their pets, create calendar events, edit information stored, add photos and even delete data. I am ecstatic and grateful to have obtained my certificate, gained so many helpful skills thus far and to start my journey into my life-long career. I am eager to pursue more knowledge, refine and begin to grasp new skills, and to be part of a team that pushes themselves to be better every day.</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <hr style={{borderTop: "3px solid #833fb2", width: "50%"}}/>
             <h5>Location</h5>
             <p>Centennial CO 80112</p>
             <h5>Phone</h5>
@@ -38,7 +38,7 @@ class Resume extends Component {
               window.location.href= "https://www.linkedin.com/in/shelby-handfield-87ba6810b/"
             }}
             >LinkedIn Profile</button>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <hr style={{borderTop: "3px solid #833fb2", width: "50%"}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
@@ -46,7 +46,7 @@ class Resume extends Component {
             <Education
               startYear={2009}
               endYear={2013}
-              schoolName="Colorado State University (CSU) "
+              schoolName="Colorado State University (CSU)"
               schoolDescription="In 2013 I obtained my diploma with a Major in Psychology and Minor in Criminology/Criminal Justice."
                />
 
@@ -56,7 +56,7 @@ class Resume extends Component {
                  schoolName="University of Denver- Full Stack Web Development Coding Bootcamp"
                  schoolDescription="Recently obtained my certificate in September 2020."
                   />
-                <hr style={{borderTop: '3px solid #e22947'}} />
+                <hr style={{borderTop: "3px solid #29e2c4"}} />
 
               <h2>Experience</h2>
 
@@ -64,7 +64,7 @@ class Resume extends Component {
               startYear={2018}
               endYear={2020}
               jobName="Social Worker"
-              jobDescription="As a Social Worker, I carried a caseload of 20-40 initial reports of either child physical, emotional and physical or sexual abuse. My role was to investigate the allegations through interviewing all family members and all potential victims. Prior to transferring, my previous role was a Child Support Enforcer. Handled a case load with families to enforce collections of payment, and maintained a 80-90% collection rate. "
+              jobDescription="As a Social Worker, I carried a caseload of 20-40 initial reports of either child physical, emotional and physical or sexual abuse. My role was to investigate the allegations through interviewing all family members and all potential victims. Prior to transferring, my previous role was a Child Support Enforcer. Handled a case load with families to enforce collections of payment, and maintained a 80-90% collection rate."
               />
 
               <Experience
@@ -73,7 +73,7 @@ class Resume extends Component {
                 jobName="District Manager"
                 jobDescription="Supervised a diverse offender pool of about 1,500 and two offices that of about 25+ employees. I ensured smooth and safe operations were being followed in both offices at Castle Rock and Centennial. From 2014 to 2016, worked as a Case Manager that worked with offenders to ensure compliance and payments were made. During 2016-2018, promoted to Assistant District Manager, and supervised 7 Case Managers while maintaining a case load of offenders as well. As a manager, ensured that case managers were trained properly, doing their daily jobs correctly and reviewing reports to ensure accuracy and integrity."
                 />
-              <hr style={{borderTop: '3px solid #e22947'}} />
+              <hr style={{borderTop: "3px solid #29e2c4"}} />
             </Cell>
         </Grid>
       </div>
